8541676101a4971c005cb3317c01718a97eccd05
BlodornN7 <122412627+BlodornN7@users.noreply.github.com>
2023-02-02 13:42:22 +0100

    Version beta du site

--- MainSite/header.php
+++ MainSite/header.php
@@ -18,7 +18,7 @@ else {echo '<header>
 			 <p> <a href="Connexionpage.php"> Se connecter </a> </p>
 			 <p> <a href="Pageinscription.php"> S\'inscrire </a> </p>
 			 </nav>	
-			 <a id="mainlogo" href="Connexionpage.php"><img src="images/logo.png" alt="Logo de l\'entreprise"></a>
+			 <a id="mainlogo" href="login.php"><img src="images/logo.png" alt="Logo de l\'entreprise"></a>
 			 
 		
 	</header>';}
